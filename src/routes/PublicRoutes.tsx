f5f4d27f5ae9068c0a148469545a14fd21d4052d
gambitier <akash.jadhav.cse@gmail.com>
2024-05-11 21:33:25 +0530

    delete landing page component

--- src/routes/PublicRoutes.tsx
+++ src/routes/PublicRoutes.tsx
@@ -1,11 +1,6 @@
-import { Landing } from "@/features/misc";
 import { RouteObject } from "react-router-dom";
 
 export const PublicRoutes: RouteObject[] = [
-  {
-    path: "/",
-    element: <Landing />,
-  },
   // import { AuthRoutes } from '@/features/auth';
   //   {
   //     path: "/auth/*",
