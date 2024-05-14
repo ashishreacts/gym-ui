522783e4fe325f1508182ddfff7ad993db4309f2
gambitier <akash.jadhav.cse@gmail.com>
2024-05-14 18:28:13 +0530

    set token in local storage

--- src/features/auth/api/Login.ts
+++ src/features/auth/api/Login.ts
@@ -1,5 +1,6 @@
 import { axiosInstance } from "@/lib/axios";
 import { useNotificationStore } from "@/stores/notifications";
+import storage from "@/utils/storage";
 import { useMutation } from "@tanstack/react-query";
 import { LoginResponseDTO } from "./types";
 
@@ -28,6 +29,7 @@ export const useLogin = () => {
       //
     },
     onSuccess: (_responseData: LoginResponseDTO, _apiParams: ApiParams) => {
+      storage.setToken(_responseData.data.entity.token.accessToken);
       addNotification({
         type: "success",
         title: "Request Successful",
--- src/features/auth/api/types/LoginResponseDTO.ts
+++ src/features/auth/api/types/LoginResponseDTO.ts
@@ -1,2 +1,32 @@
 import { APIResponse } from "@/types/api";
-export type LoginResponseDTO = APIResponse<unknown>;
+export type LoginResponseDTO = APIResponse<LoginApiResponseData>;
+
+export type LoginApiResponseData = {
+  entity: Entity;
+};
+
+export type Entity = {
+  user: User;
+  token: Token;
+};
+
+export type Token = {
+  accessToken: string;
+};
+
+export type User = {
+  id: string;
+  prefix: string;
+  firstName: string;
+  lastName: string;
+  phone: string;
+  email: string;
+  userRoles: UserRole[];
+};
+
+export type UserRole = {
+  id: string;
+  role: string;
+  userId: string;
+  createdAt: Date;
+};
