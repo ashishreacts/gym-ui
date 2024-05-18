df0a41717b4419ce3f989618b945b49b77c44f45
AJ5154 <ashishjadhav597@gmail.com>
2024-05-18 13:26:06 +0530

    Add Authindicator component

--- src/routes/ProtectedRoutes.tsx
+++ src/routes/ProtectedRoutes.tsx
@@ -2,11 +2,16 @@ import { BatchRoutes } from "@/features/batches";
 import { PlanRoutes } from "@/features/plans";
 import { Navigate, RouteObject } from "react-router-dom";
 import { MainLayoutWrapper } from "./MainLayoutWrapper";
+import { Authenticator } from "@/features/auth";
 
 export const ProtectedRoutes: RouteObject[] = [
   {
     path: "/app",
-    element: <MainLayoutWrapper />,
+    element: (
+      <Authenticator>
+        <MainLayoutWrapper />
+      </Authenticator>
+    ),
     children: [
       { path: "", element: <Navigate to="/app/plans" /> }, // Redirect /app to /app/plans
       { path: "/app/plans/*", element: <PlanRoutes /> },
--- src/routes/PublicRoutes.tsx
+++ src/routes/PublicRoutes.tsx
@@ -1,7 +1,12 @@
 import { AuthRoutes } from "@/features/auth";
+import { Typography } from "@mui/material";
 import { RouteObject } from "react-router-dom";
 
 export const PublicRoutes: RouteObject[] = [
+  {
+    path: "",
+    element: <Typography variant="h1">Welcome!</Typography>,
+  },
   {
     path: "/auth/*",
     element: <AuthRoutes />,
